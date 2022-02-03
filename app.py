5fd9954683b3eef5c34d07144f6494bd682b906b
tkirk70 <timothypatrickkirk@gmail.com>
2022-02-03 15:54:34 -0500

    Module 10 Challenge Deliverables.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
@@ -12,7 +12,7 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
-   return render_template("challenge_index.html", mars=mars)
+   return render_template("index.html", mars=mars)
 
 
 @app.route("/scrape")
